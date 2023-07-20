22378d3f108d6104f67f2528795183768b97df5e
zewdu erkyhun <zewdu444@gmail.com>
2023-07-20 20:03:15 +0300

    Add SEO meta tage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 import './index.css';
 import App from './App';
 import '@fontsource/lato/100.css';
@@ -9,12 +10,20 @@ import '@fontsource/lato/700.css';
 import '@fontsource/lato/900.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const title = 'Zewdu Anley';
+const description = 'Zewdu Anley Portfolio page';
+const keywords = 'zewdu, Erkyhun, Anley';
+
 root.render(
-
-  <App />,
-
+  <div>
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="robots" content="index, follow" />
+      {/* Add other meta tags as needed */}
+    </Helmet>
+    <App />
+  </div>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
